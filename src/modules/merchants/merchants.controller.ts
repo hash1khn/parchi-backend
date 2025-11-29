212e90e12f513adf8cffef7e2a79fd4539220e3c
Aawaiz <aawaizali8@gmail.com>
2025-11-29 15:52:47 +0500

    Revert "feat: Implement admin and corporate merchant branch management and merchant update functionalities with new DTOs."
    This reverts commit 4a72540dfccb1074145c99ac6dc5708c8c669123.

--- src/modules/merchants/merchants.controller.ts
+++ src/modules/merchants/merchants.controller.ts
@@ -1,15 +1,10 @@
 import {
-  Patch,
-  Param,
-  Body,
   Controller,
   Get,
   HttpCode,
   HttpStatus,
   UseGuards,
 } from '@nestjs/common';
-import { CurrentUser } from '../../decorators/current-user.decorator';
-import { UpdateBranchDto } from './dto/update-branch.dto';
 import { MerchantsService } from './merchants.service';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
 import { RolesGuard } from '../../common/guards/roles.guard';
@@ -26,32 +21,6 @@ export class MerchantsController {
   @HttpCode(HttpStatus.OK)
   async getAllCorporateMerchants() {
     return this.merchantsService.getAllCorporateMerchants();
-  }
-  @Get(':id/branches')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(ROLES.ADMIN, ROLES.MERCHANT_CORPORATE)
-  @HttpCode(HttpStatus.OK)
-  async getMerchantBranches(
-    @Param('id') id: string,
-    @CurrentUser() user: any,
-  ) {
-    return this.merchantsService.getMerchantBranches(id, user);
-  }
-
-  @Patch('branches/:id')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(ROLES.MERCHANT_CORPORATE)
-  @HttpCode(HttpStatus.OK)
-  async updateBranch(
-    @Param('id') id: string,
-    @Body() updateBranchDto: UpdateBranchDto,
-    @CurrentUser() user: any,
-  ) {
-    return this.merchantsService.updateBranchByCorporate(
-      id,
-      updateBranchDto,
-      user,
-    );
   }
 }
 
--- src/modules/merchants/merchants.module.ts
+++ src/modules/merchants/merchants.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MerchantsController } from './merchants.controller';
-import { AdminMerchantsController } from './admin-merchants.controller';
 import { MerchantsService } from './merchants.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [PrismaModule, AuthModule],
-  controllers: [MerchantsController, AdminMerchantsController],
+  controllers: [MerchantsController],
   providers: [MerchantsService],
   exports: [MerchantsService],
 })
