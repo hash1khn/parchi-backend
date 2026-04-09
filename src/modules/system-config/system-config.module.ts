c86edc13040f719d67236260f6c5a4e20d2902a5
Aawaiz <aawaizali8@gmail.com>
2026-04-09 17:08:41 +0500

    jee hashir bhai

--- src/modules/system-config/system-config.module.ts
+++ src/modules/system-config/system-config.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { SystemConfigController } from './system-config.controller';
 import { SystemConfigService } from './system-config.service';
 
+import { AuthModule } from '../auth/auth.module';
+import { PrismaModule } from '../prisma/prisma.module';
+
 @Module({
+  imports: [PrismaModule, AuthModule],
   controllers: [SystemConfigController],
   providers: [SystemConfigService],
   exports: [SystemConfigService],
